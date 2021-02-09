b8fff878afa215ce0b36a8245a0192272d18d3fc
Felix Pohlmann <felixpohlmann.dev@gmail.com>
2021-02-09 13:25:16 +0100

    minor changes

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -4,11 +4,11 @@ const authConfig = require("../config/auth.config");
 function checkAuth(req, res, next) {
   const token = req.cookies.token;
   if (!token) {
-    res.send("Unauthorized: No token provided!").status("401");
+    res.send("No token provided!").end();
   } else {
     jwt.verify(token, authConfig.tokenSecret, (err, decoded) => {
       if (err) {
-        res.send("Invalid token!");
+        res.send("Invalid token").end();
       } else {
         //successful login
         //calling next route
